86553a83799ca8d44ae259b9ff3691ebd9356149
Dominik Kulpowicz <dominik.kulpowicz@gmail.com>
2022-10-04 00:45:03 +0200

    First Commit

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -1,2 +1,17 @@
-package com.kodilla;public class Calculator {
+package com.kodilla;
+
+public class Calculator {
+
+public int plusMethod(int a, int b){
+    return a + b;
+}
+public int minusMethod(int a, int b){
+    return a - b;
+}
+
+public static void main(String args []){
+    Calculator calculator = new Calculator();
+    int result = calculator.plusMethod(5, 8);
+    System.out.println(result);
+}
 }
